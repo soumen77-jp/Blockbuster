8690578e4121173ce6aaf6569360046b14e3b014
Bチーム <h1202415004@hiratagakuen.onmicrosoft.com>
2024-10-18 12:00:55 +0900

    メインシーン作成

---
+++ Inbelurer/Assets/otsuka/Stage1/BulletController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 10f;
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1) && currentAmmo > 0)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+       GameObject bullet = Instantiate(bulletPrefab, firePoint. position, firePoint.rotation);
+        currentAmmo--;
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = firePoint.up * bulletSpeed;
+       
+    }
+
+    public void AddAmmo(int amount)
+    {
+        currentAmmo = Mathf.Min(currentAmmo + amount, maxAmmo);
+    }
+}
