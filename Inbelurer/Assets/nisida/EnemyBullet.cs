4cc07e31ce24a07e885be8b4d20f1a45aaa8c536
kawasakiryota <h1202419005@hiratagakuen.onmicrosoft.com>
2024-11-05 11:29:12 +0900

    no message

--- Inbelurer/Assets/nisida/EnemyBullet.cs
+++ Inbelurer/Assets/nisida/EnemyBullet.cs
@@ -1,33 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
-public class BossEnemyBullet : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
-    // 設定された方向に弾を移動させる
-    float dx;
-    float dy;
-    public void Setting(float angle, float speed)
-    {
-        // 敵の右側が0°
-        // 反時計回りに角度は増える
-
-        // 2PIが360°
-        // PIが180°
-        // PI/2が90°
-
-        dx = Mathf.Cos(angle) * speed;
-        dy = Mathf.Sin(angle) * speed;
-    }
+    public float speed = 5f;
 
     void Update()
     {
-        transform.position += new Vector3(dx, dy, 0) * Time.deltaTime;
-
-        if (transform.position.x < -3 || transform.position.x > 3 ||
-            transform.position.y < -3 || transform.position.y > 3)
-        {
-            Destroy(gameObject);
-        }
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        Debug.Log(1);
+        //プレイヤーにダメージを与える処理をここに追加
+        Destroy(other.gameObject);
+
+    }
+    
+
 }
