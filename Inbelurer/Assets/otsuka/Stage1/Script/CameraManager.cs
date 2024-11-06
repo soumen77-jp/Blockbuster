cf1052d50f8b157921ba9a3a3dc35b1862b9bc99
Bチーム <h1202415004@hiratagakuen.onmicrosoft.com>
2024-11-06 11:40:47 +0900

    スクロール作成

--- Inbelurer/Assets/otsuka/Stage1/Script/CameraManager.cs
+++ Inbelurer/Assets/otsuka/Stage1/Script/CameraManager.cs
@@ -4,52 +4,17 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public float leftLimit = 0.0f; //左スクロールリミット
-    public float rightLimit = 0.0f; //右スクロールリミット
-    public float topLimit = 0.0f; //上スクロールリミット
-    public float bottomLimit = 0.0f; //下スクロールリミット
+    public Transform player; // プレイヤーのTransform
+    public float offsetY = 2f; // カメラの少し下の位置を設定
+    public float scrollLimit = 10f; // スクロールを止める位置
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        GameObject player =
-            GameObject.FindGameObjectWithTag("Player"); //プレイヤーを探す
-        if(player != null)
+        // プレイヤーが上に移動しているかを確認
+        if (player.position.y > transform.position.y - offsetY && transform.position.y < scrollLimit)
         {
-            //カメラの更新座標
-            float x = player.transform.position.x;
-            float y = player.transform.position.y;
-            float z = transform.position.z;
-            //横同期させる
-            //両端に移動制限を付ける
-            if(x < leftLimit)
-            {
-                x = leftLimit;
-            }
-            else if (x > rightLimit)
-            {
-                x = rightLimit;
-            }
-            //縦同期させる
-            //上下に移動制限を付ける
-            if(y < bottomLimit)
-            {
-                y = bottomLimit;
-            }
-            else if (y > topLimit)
-            {
-                y = topLimit;
-            }
-            //カメラ位置のVector3を作る
-            Vector3 v3 = new Vector3(x, y, z);
-            transform.position = v3;
+            Vector3 newPosition = new Vector3(transform.position.x, player.position.y + offsetY, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime);
         }
-
     }
 }
